2db75de240c5fb91157fd472f88570a04eec363e
Reeve <dcunhareeve@gmail.com>
2021-03-10 19:22:55 +1300

    Add initial Top Bar Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import TopBar from './Components/TopBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <div style={{ width: '80%' }}>
+          <TopBar userName='Reeve' role='Civilian' />
+        </div>
       </header>
     </div>
   );
