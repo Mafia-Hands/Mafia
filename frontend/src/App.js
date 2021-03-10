7525d96b01f6204988de5352a56d1c9b1d17730f
Alan.Thomas <alanunny@outlook.com>
2021-03-11 00:14:56 +1300

    added core functionality for table/player

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,83 @@
-import './App.css';
-import TopBar from './Components/TopBar';
-import GameEnv from './Components/GameEnv';
-import BottomBar from './Components/BottomBar';
+import './App.css'
+import TopBar from './Components/TopBar'
+import GameEnv from './Components/GameEnv'
+import BottomBar from './Components/BottomBar'
 
 function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <div style={{ width: '80%' }}>
-          <TopBar userName='Reeve' role='Civilian' />
-          <GameEnv />
-          <BottomBar />
+    const playerStates = [
+        {
+            playerId: '1',
+            name: 'Player 1',
+            state: 'alive',
+        },
+        {
+            playerId: '2',
+            name: 'Player 2',
+            state: 'alive',
+        },
+        {
+            playerId: '3',
+            name: 'Player 3',
+            state: 'alive',
+        },
+        {
+            playerId: '4',
+            name: 'Player 4',
+            state: 'alive',
+        },
+        {
+            playerId: '5',
+            name: 'Player 5',
+            state: 'alive',
+        },
+        {
+            playerId: '6',
+            name: 'Player 6',
+            state: 'alive',
+        },
+        {
+            playerId: '7',
+            name: 'Player 6',
+            state: 'alive',
+        },
+        // {
+        //     playerId: "8",
+        //     name: "Player 6",
+        //     state: "alive",
+        // },
+        // {
+        //     playerId: "9",
+        //     name: "Player 6",
+        //     state: "alive",
+        // },
+        // {
+        //     playerId: "10",
+        //     name: "Player 6",
+        //     state: "alive",
+        // },
+        // {
+        //     playerId: "11",
+        //     name: "Player 6",
+        //     state: "alive",
+        // },
+        // {
+        //     playerId: "12",
+        //     name: "Player 6",
+        //     state: "alive",
+        // },
+    ]
+    return (
+        <div className="App">
+            <header className="App-header">
+                <div style={{ width: '80%' }}>
+                    <TopBar userName="Reeve" role="Civilian" />
+                    <GameEnv />
+
+                    <BottomBar />
+                </div>
+            </header>
         </div>
-      </header>
-    </div>
-  );
+    )
 }
 
-export default App;
+export default App
