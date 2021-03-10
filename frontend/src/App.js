31d84d78e47dfeb20bff89756b0651df1b56d3d5
Alan.Thomas <alanunny@outlook.com>
2021-03-11 11:22:10 +1300

    finished most functionality for table/player

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,68 +4,6 @@ import GameEnv from './Components/GameEnv'
 import BottomBar from './Components/BottomBar'
 
 function App() {
-    const playerStates = [
-        {
-            playerId: '1',
-            name: 'Player 1',
-            state: 'alive',
-        },
-        {
-            playerId: '2',
-            name: 'Player 2',
-            state: 'alive',
-        },
-        {
-            playerId: '3',
-            name: 'Player 3',
-            state: 'alive',
-        },
-        {
-            playerId: '4',
-            name: 'Player 4',
-            state: 'alive',
-        },
-        {
-            playerId: '5',
-            name: 'Player 5',
-            state: 'alive',
-        },
-        {
-            playerId: '6',
-            name: 'Player 6',
-            state: 'alive',
-        },
-        {
-            playerId: '7',
-            name: 'Player 6',
-            state: 'alive',
-        },
-        // {
-        //     playerId: "8",
-        //     name: "Player 6",
-        //     state: "alive",
-        // },
-        // {
-        //     playerId: "9",
-        //     name: "Player 6",
-        //     state: "alive",
-        // },
-        // {
-        //     playerId: "10",
-        //     name: "Player 6",
-        //     state: "alive",
-        // },
-        // {
-        //     playerId: "11",
-        //     name: "Player 6",
-        //     state: "alive",
-        // },
-        // {
-        //     playerId: "12",
-        //     name: "Player 6",
-        //     state: "alive",
-        // },
-    ]
     return (
         <div className="App">
             <header className="App-header">
