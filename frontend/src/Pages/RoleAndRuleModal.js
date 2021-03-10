76930501d0b1671a6ee5bc0c18d29efb73439c79
Kevin Ge <gjw1047340859@gmail.com>
2021-03-10 21:05:47 +1300

    Put the "go back" button within the Modal

--- frontend/src/Pages/RoleAndRuleModal.js
+++ frontend/src/Pages/RoleAndRuleModal.js
@@ -2,14 +2,15 @@ import React from 'react';
 import RoleTable from '../Components/RoleTable.js';
 import RuleParagraph from '../Components/RuleParagraph';
 import styles from '../Styles/RoleAndRuleModal.module.css';
-import Header from '../Components/Header';
+//import Header from '../Components/Header';
 
 const RoleAndRuleModal = () => {
 
     return (
         <div className={styles.gradient}>
 
-            <Header />
+            {/* <Header /> */}
+            <button > Go back </button>
            
             <div>
                 <b> Mafia Roles </b>
