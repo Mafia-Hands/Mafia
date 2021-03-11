a3029af4f0986c6b2b5f47b0e6e724bd2fe6a6ea
Kevin Ge <gjw1047340859@gmail.com>
2021-03-12 12:21:20 +1300

    setting modal done

--- frontend/src/Components/TopBar.jsx
+++ frontend/src/Components/TopBar.jsx
@@ -6,7 +6,7 @@ import styles from '../Styles/TopBar.module.css';
 
 const TopBar = ({ userName, role }) => {
     const [open, setOpen] = useState(false);
-
+    const [openInfo, setOpenInfo] = useState(false);
     // const [openInfo, setOpenInfo] = useState(false);
 
     // const [openSetting, setOpenSetting] = useState(false);
@@ -44,20 +44,38 @@ const TopBar = ({ userName, role }) => {
         // </div>
 
         // <ModalMUI info={openInfo} setInfo={setOpenInfo} setting={openSetting} setSetting={setOpenSetting}>
+
         <div className={styles.container}>
             <p className={styles.userName}>{`Name: ${userName}`}</p>
             <p className={styles.userRole}>
                 {`Role: ${role}`}
-                <button onClick={() => setOpen(true)}>
+                <button
+                    onClick={() => {
+                        setOpen(true);
+                        setOpenInfo(true);
+                    }}
+                >
                     <i className="fa fa-info"></i>
                 </button>
             </p>
             <div className={styles.timer}>Timer Placeholder</div>
 
-            <button className={styles.settingsButton}>Settings</button>
+            <button
+                className={styles.settingsButton}
+                onClick={() => {
+                    setOpen(true);
+                    setOpenInfo(false);
+                }}
+            >
+                Settings
+            </button>
 
             <ModalMUI open={open} setOpen={setOpen}>
-                <RoleAndRuleDialog goback={setOpen} />
+                {openInfo ? (
+                    <RoleAndRuleDialog goback={setOpen} />
+                ) : (
+                    <SettingDialog goback={setOpen} />
+                )}
             </ModalMUI>
         </div>
     );
--- frontend/src/Pages/SettingDialog.js
+++ frontend/src/Pages/SettingDialog.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import GoBackBtn from '../Components/GoBackButton';
-import Modal from '../Modal/Modal';
 import styles from '../Styles/SettingDialog.module.css';
 import Slider from '../Components/Slider';
 
 const SettingDialog = (props) => {
     return (
-        <Modal dismissOnClickOutside={true} onCancel={props.goback}>
-            <div className={styles.modal}>
-                <GoBackBtn goback={props.goback} />
+        <div className={styles.modal}>
+            <GoBackBtn goback={props.goback} />
 
-                <div>
-                    <b> Settings </b>
-                </div>
-
-                <div>
-                    <b> Sound </b>
-                    <Slider />
-                </div>
+            <div>
+                <h2> Settings </h2>
             </div>
-        </Modal>
+
+            <div>
+                <b> Sound </b>
+                <Slider />
+            </div>
+
+            <div>
+                <b> Brightness </b>
+                <Slider />
+            </div>
+        </div>
     );
 };
 
