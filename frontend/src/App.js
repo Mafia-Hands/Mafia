74701104fc260a1da55e5c303703ea7c88bbe347
Kevin Ge <gjw1047340859@gmail.com>
2021-03-12 12:23:21 +1300

    remove unused files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,39 +2,9 @@ import './App.css';
 import TopBar from './Components/TopBar';
 import GameEnv from './Components/GameEnv';
 import BottomBar from './Components/BottomBar';
-import RuleAndRuleDialog from './Pages/RoleAndRuleDialog';
-import { Switch, Route } from 'react-router-dom';
-import SettingDialog from './Pages/SettingDialog';
-import { useState } from 'react';
 
 function App() {
-    
-
     return (
-        // <Switch>
-
-        //   <Route path="/info">
-        //       <RuleAndRuleDialog />
-        //   </Route>
-
-        //   <Route path="/setting">
-        //       <SettingDialog />
-        //   </Route>
-
-        //   <Route path="/">
-        //     <div className='App'>
-        //       <header className='App-header'>
-        //           <div style={{ width: '80%' }}>
-        //             <TopBar userName='Reeve' role='Civilian' />
-        //             <GameEnv />
-        //             <BottomBar />
-        //           </div>
-        //         </header>
-        //       </div>
-        //   </Route>
-
-        // </Switch>
-
         <div className="App">
             <header className="App-header">
                 <div style={{ width: '80%' }}>
