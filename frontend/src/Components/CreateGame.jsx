808406d015376320bd62742c16c912719241a90f
SkyN <49630838+skyn310198@users.noreply.github.com>
2021-03-11 21:14:36 +1300

    Create CreateGame button to create new lobby

---
+++ frontend/src/Components/CreateGame.jsx
@@ -0,0 +1,12 @@
+import  React, {useState} from 'react';
+
+const CreateGame = () => {
+    return (
+        <div>
+            <button> JOIN </button>
+            /* button will have onClick to create a new Lobby */
+        </div>
+    );
+};
+
+export default CreateGame; 
