d41f75a4a7d4e9a32e80d3a8b0c326328bcd71e6
LinDaBot <voidnullbot@outlook.com>
2021-03-12 00:10:26 +1300

    2nd out of ???? attempts for CORS

--- backend/index.js
+++ backend/index.js
@@ -2,15 +2,12 @@ const app = require("express")();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server, {
     // Set up of CORS settings for socket.io server
+    // Reason for all site access is for the ease of development, since we might have various local/cloud website setup for testing purposes.
+    // While allowing all origins may pose a security thread for a banking website, as any site can make a request using the same session
+    // This won't be a problem in our website, as there are no local session/cookies returned to the user. 
+    // Please refer to the post: https://stackoverflow.com/questions/12001269/what-are-the-security-risks-of-setting-access-control-allow-origin
     cors: {
-        origin: [
-            "*" // Allow all origin to access the website's data
-            // Reason for this is for the ease of development, since we might have various local/cloud website setup for testing purposes.
-            // While allowing all origins may pose a security thread for a banking website, as any site can make a request using the same session
-            // This won't be a problem in our website, as there are no local session/cookies returned to the user. 
-            // Please refer to the post: https://stackoverflow.com/questions/12001269/what-are-the-security-risks-of-setting-access-control-allow-origin
-        ],
-        methods: ["GET", "POST"]
+        origin: '*', // Allow all origin to connect to the website
     }
 });
 const port = process.env.PORT || 4001;
