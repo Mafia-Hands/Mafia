05bc9e691561d371c7a4fe07a9b2cb1cafeaeb77
ateagit <43104158+ateagit@users.noreply.github.com>
2021-03-11 20:21:49 +1300

    Feature/code formatting (#47)
    * added prettier config and pretty-quick
    
    * ran prettier for all src files

--- frontend/src/Components/TopBar.jsx
+++ frontend/src/Components/TopBar.jsx
@@ -2,14 +2,14 @@ import { React } from 'react';
 import styles from '../Styles/TopBar.module.css';
 
 const TopBar = ({ userName, role }) => {
-  return (
-    <div className={styles.container}>
-      <p className={styles.userName}>{`Name: ${userName}`}</p>
-      <p className={styles.userRole}>{`Role: ${role}`}</p>
-      <div className={styles.timer}>Timer Placeholder</div>
-      <button className={styles.settingsButton}>Settings</button>
-    </div>
-  );
+    return (
+        <div className={styles.container}>
+            <p className={styles.userName}>{`Name: ${userName}`}</p>
+            <p className={styles.userRole}>{`Role: ${role}`}</p>
+            <div className={styles.timer}>Timer Placeholder</div>
+            <button className={styles.settingsButton}>Settings</button>
+        </div>
+    );
 };
 
 export default TopBar;
