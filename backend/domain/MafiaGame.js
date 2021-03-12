f580f3321837fe2d3b4357aed9a01111bf53afff
dh <45830988+Dhors@users.noreply.github.com>
2021-03-12 14:55:19 +1300

    Domain models for the mafia game added. (#52)
    * Daniel + Justin - we added the domain models for the mafia game. We also added some basic funtionality for the classes.
    
    * Fixes issues outlined in pull request. Specifically whitespace consistency issues and changed to class based constructers.
    
    * changed function constructors into class constructors and file into individual files for each class. Also did small bugfixing
    
    * capitalised the enum names for gamestates
    
    * added export statements and added some more meaningful names

---
+++ backend/domain/MafiaGame.js
@@ -0,0 +1,19 @@
+class MafiaGame {
+    constructor() {
+        // Acts  as a mapping for gameID keys to their gameroom.
+        const gameRoomsDict = {};
+    }
+
+    newGame() {
+        let newRoom = new Room();
+        let gameID = newRoom.getRoomName();
+        gameRoomsDict[gameID] = newRoom;
+    }
+
+    closeGame(gameID) {
+        delete gameRoomsDict[gameID];
+    }
+
+}
+
+module.export = MafiaGame;
