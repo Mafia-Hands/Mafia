cbb099b95c4c0ad9a1be887fef45c69d02fb3acb
SkyN <49630838+skyn310198@users.noreply.github.com>
2021-03-14 12:31:48 +1300

    Home Screen is Created with Material UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,12 @@
 import './App.css';
-import TopBarGame from './Components/TopBarGame';
-import GameEnv from './Components/GameEnv';
-import BottomBar from './Components/BottomBar';
+import HomeScreen from './Components/HomeScreen';
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
                 <div style={{ width: '80%' }}>
-                    <TopBarGame
-                        userDetails={['Reeve', 'Civilian']}
-                        showTimer={true}
-                        showRole={true}
-                    />
-                    <GameEnv />
-                    <BottomBar />
+                    <HomeScreen/>
                 </div>
             </header>
         </div>
---
+++ frontend/src/Components/HomeScreen.jsx
@@ -0,0 +1,47 @@
+import { React, useState } from 'react';
+import CodeJoinBar from '../Components/CodeJoinBar';
+import ModalMUI from '../Modal/ModalMUI';
+import NicknameBar from '../Components/NicknameBar';
+import CreateGame from '../Components/CreateGame';
+import SettingDialog from '../Pages/SettingDialog';
+import TopBarSettings from './TopBarSettings';
+import { Grid, Toolbar } from '@material-ui/core';
+
+
+const HomeScreen = (userDetails) => {
+    const [open, setOpen] = useState(false);
+    
+    return(
+        <Grid container direction= "column">
+            
+            <Toolbar>
+                <button
+                        onClick={() => {
+                            setOpen(true);
+                        }}
+                    >
+                        Settings
+                </button>
+            </Toolbar>
+            <Grid item><h1>MAFIA</h1></Grid>
+
+            <div>
+                <ModalMUI open={open} setOpen={setOpen}>
+                    <div>
+                        <TopBarSettings
+                            showUp={setOpen}
+                            currentScreen="HOME"
+                            showSettings={false}
+                    />
+                    <SettingDialog />
+                    </div>
+                </ModalMUI>
+            </div>
+            <Grid item><CodeJoinBar/></Grid>
+            <Grid item><NicknameBar/></Grid>
+            <Grid item><CreateGame/></Grid>
+        </Grid>
+    );
+};
+
+export default HomeScreen;
