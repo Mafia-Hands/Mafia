c66609329d8b69d083639ea5d18cf604f6adce54
Reeve <dcunhareeve@gmail.com>
2021-03-14 04:04:21 +1300

    Switch to Material UI sliders

--- frontend/src/Pages/SettingDialog.js
+++ frontend/src/Pages/SettingDialog.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import styles from '../Styles/SettingDialog.module.css';
-import Slider from '../Components/Slider';
-import { Button } from '@material-ui/core';
+import { Slider, Button } from '@material-ui/core';
 
 const SettingDialog = () => {
     return (
-        <div className={styles.modal}>
+        <div className={styles.container}>
             <div>
                 <b> Sound </b>
                 <Slider />
@@ -17,8 +16,10 @@ const SettingDialog = () => {
             </div>
 
             {/* TODO: improve CSS and layout */}
-            <div className={styles.leaveBtn}>
-                <Button variant="contained"> Leave Game </Button>
+            <div>
+                <Button variant="contained" style={{ textAlign: 'center' }}>
+                    Leave Game
+                </Button>
             </div>
         </div>
     );
