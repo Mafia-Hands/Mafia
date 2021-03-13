966c0854752359542238771bf68df489dca99f5b
LinDaBot <voidnullbot@outlook.com>
2021-03-13 20:32:32 +1300

    Update documentation

--- backend/Events/LobbyEvents.js
+++ backend/Events/LobbyEvents.js
@@ -1,6 +1,14 @@
 const Player = require("../domain/Player");
 const LobbyCodeDTO = require("../domain/DTO/response/LobbyCodeDTO");
+const MafiaGame = require("../domain/MafiaGame");
 
+/**
+ * Event handlers and logic for `create-lobby` and `lobby-code`
+ * The goal of these lobby events is to allow a host to create a game and receive a new room id. 
+ * @param {any} io 
+ * @param {any} socket 
+ * @param {MafiaGame} mafiaGame 
+ */
 exports.load_lobby_events = function(io, socket, mafiaGame){
     socket.on("create-lobby", (createLobbyDTO) => {
         console.log("New room request received");
