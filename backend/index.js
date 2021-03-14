8da35190bf2d160a995ff0bf90002895f8a821d7
Sreeniketh Raghavan <sreeniketh0609@gmail.com>
2021-03-15 11:05:42 +1300

    Updated index.js to redirect start-game events to the newly added handler

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,20 @@
-const app = require('express')();
-const server = require('http').createServer(app);
-const config = require('./config.json');
-const MafiaGame = require('./domain/MafiaGame');
+const app = require("express")();
+const server = require("http").createServer(app);
+const config = require("./config.json");
+const MafiaGame = require("./domain/MafiaGame");
 
-const loadLobbyEvents = require('./Events/LobbyEvents');
+const loadLobbyEvents = require("./Events/LobbyEvents");
+const loadGameStartEvents = require("./Events/GameStartEvents");
 
-const io = require('socket.io')(server, {
-    // Set up of CORS settings for socket.io server
-    // Reason for all site access is for the ease of development, since we might have various local/cloud website setup for testing purposes.
-    // While allowing all origins may pose a security thread for a banking website, as any site can make a request using the same session
-    // This won't be a problem in our website, as there are no local session/cookies returned to the user.
-    // Please refer to the post: https://stackoverflow.com/questions/12001269/what-are-the-security-risks-of-setting-access-control-allow-origin
-    cors: {
-        origin: config.cors_origin, // Allow all origin to connect to the website
-    },
+const io = require("socket.io")(server, {
+  // Set up of CORS settings for socket.io server
+  // Reason for all site access is for the ease of development, since we might have various local/cloud website setup for testing purposes.
+  // While allowing all origins may pose a security thread for a banking website, as any site can make a request using the same session
+  // This won't be a problem in our website, as there are no local session/cookies returned to the user.
+  // Please refer to the post: https://stackoverflow.com/questions/12001269/what-are-the-security-risks-of-setting-access-control-allow-origin
+  cors: {
+    origin: config.cors_origin, // Allow all origin to connect to the website
+  },
 });
 
 const port = process.env.PORT || config.local_port;
@@ -25,8 +26,9 @@ app.get('/', (req, res) => {
 });
 
 // Listen for a "connection" event for incoming sockets.
-io.on('connection', (socket) => {
-    loadLobbyEvents(io, socket, mafiaGame);
+io.on("connection", (socket) => {
+  loadLobbyEvents(io, socket, mafiaGame);
+  loadGameStartEvents(socket, mafiaGame);
 });
 
 // Start the server on our predetermined port number.
