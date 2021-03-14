c7cebbc6aeb0b7fc6c1580620b815a7b752e9219
SkyN <49630838+skyn310198@users.noreply.github.com>
2021-03-14 22:38:10 +1300

    Use TopBar component for the HomeScreenID instead

--- frontend/src/Components/HomeScreenID.jsx
+++ frontend/src/Components/HomeScreenID.jsx
@@ -1,40 +1,38 @@
 import { React, useState } from 'react';
+import CodeJoinBar from '../Components/CodeJoinBar';
 import ModalMUI from '../Modal/ModalMUI';
 import NicknameBar from '../Components/NicknameBar';
+import CreateGame from '../Components/CreateGame';
 import SettingDialog from '../Pages/SettingDialog';
 import TopBarSettings from './TopBarSettings';
-import { Grid, Toolbar } from '@material-ui/core';
+import SettingsIcon from '@material-ui/icons/Settings';
+import { IconButton } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
 
 
 const HomeScreenID = () => {
     const [open, setOpen] = useState(false);
     
     return(
-        <Grid container direction= "column">
-            
-            <Toolbar>
-                <button
+        
+        <Grid container direction= "column" justify="space-evenly" spacing="2">
+            <Grid item container direction="row" >
+                <Grid item xs ={0} sm={2}/>
+                <Grid item xs ={10} sm={8}><h1>MAFIA</h1></Grid>
+                <Grid item xs ={0} sm={2}>
+                    <IconButton
                         onClick={() => {
                             setOpen(true);
                         }}
                     >
-                        Settings
-                </button>
-            </Toolbar>
-            <Grid item><h1>MAFIA</h1></Grid>
-
-            <div>
-                <ModalMUI open={open} setOpen={setOpen}>
-                    <div>
-                        <TopBarSettings
-                            showUp={setOpen}
-                            currentScreen="HOME"
-                            showSettings={false}
-                    />
-                    <SettingDialog />
-                    </div>
-                </ModalMUI>
-            </div>
+                        <SettingsIcon/>
+                    </IconButton>
+                        <ModalMUI open={open} setOpen={setOpen}>
+                        <TopBarSettings showUp={false} currentScreen="HOME" showSettings={false}/>
+                        <SettingDialog />
+                        </ModalMUI>
+                </Grid>
+            </Grid>
             <Grid item><NicknameBar/></Grid>
         </Grid>
     );
