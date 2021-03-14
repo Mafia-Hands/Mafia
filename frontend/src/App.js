7d2bbaf801dedaa32d3d47ce1eacbd26261c1f40
skyn310198 <49630838+skyn310198@users.noreply.github.com>
2021-03-15 00:23:26 +1300

    Home screen combine (#91)
    * Home Screen is Created with Material UI
    
    * Add HomeScreenID for when user enter the game with /{id}
    
    * add prototype for Nickname bar functionality
    
    * minor fix
    
    * replace current component with Material UI
    
    * Use TopBar component for the HomeScreen instead
    
    * Use TopBar component for the HomeScreenID instead
    
    * delete unused var

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,12 @@
 import './App.css';
-import TopBarGame from './Components/TopBarGame';
-import GameEnv from './Components/GameEnv';
-import BottomBar from './Components/BottomBar';
+import HomeScreenID from './Components/HomeScreenID';
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
                 <div style={{ width: '80%' }}>
-                    <TopBarGame
-                        userDetails={['Reeve', 'Civilian']}
-                        showTimer={true}
-                        showRole={true}
-                    />
-                    <GameEnv />
-                    <BottomBar />
+                    <HomeScreenID/>
                 </div>
             </header>
         </div>
