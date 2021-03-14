959502f49b40ff7e41e13f4f07870b687410ddb7
Seif Younes <seif99y@gmail.com>
2021-03-14 17:19:01 +1300

    Added JSDoc comments to nightTimeVotingEvents

--- backend/Events/nightTimeVoteEvents.js
+++ backend/Events/nightTimeVoteEvents.js
@@ -2,9 +2,20 @@ const RoleEnum = require('../domain/Enum/Role');
 const SuspectRevealDTO = require('../domain/DTO/response/SuspectRevealDTO');
 
 /**
- *
+ * Event handlers and logic for `mafia-vote`, 'medic-vote' and `detective-vote`
+ * This involves the functionality for night-time voting, which includes:
+ * - Mafia choosing who to kill before the next round
+ * - Medics choosing who to save
+ * - Detectives choosing who to suspect
+ * @param {any} io
+ * @param {any} socket
+ * @param {MafiaGame} mafiaGame
  */
 exports.nightTimeEvents = (io, socket, mafiaGame) => {
+    /**
+     * Handler for 'mafia-vote', sets the chosen player for the Mafia to the player specified in the message.
+     * In future iterations, this will be changed to tally votes from mafia members before setting the chosen player.
+     */
     socket.on('mafia-vote', (mafiaVoteObj) => {
         let room = socket.player.roomID;
         room.mafiaChosenPlayer = room.getPlayerByNickname(
@@ -12,6 +23,9 @@ exports.nightTimeEvents = (io, socket, mafiaGame) => {
         );
     });
 
+    /**
+     * Handler for 'medic-vote', pretty much the same logic as the mafia vote, except it sets the chosen player for the Medics.
+     */
     socket.on('medic-vote', (medicVoteObj) => {
         let room = socket.player.roomID;
         room.medicChosenPlayer = room.getPlayerByNickname(
@@ -19,6 +33,10 @@ exports.nightTimeEvents = (io, socket, mafiaGame) => {
         );
     });
 
+    /**
+     * Handler for detective vote. Retrieves the player specified in the message, checks their role, and replies with a
+     * SuspectRevealDTO that reveals whether the chosen player is Mafia or not.
+     */
     socket.on('detective-vote', (detectiveVoteObj) => {
         let room = socket.player.roomID;
 
