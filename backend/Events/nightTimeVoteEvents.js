42c6af99c893c2ecae978a23e77797db6b9fa170
Seif Younes <seif99y@gmail.com>
2021-03-14 16:59:05 +1300

    Changed method of getting player's room

--- backend/Events/nightTimeVoteEvents.js
+++ backend/Events/nightTimeVoteEvents.js
@@ -6,22 +6,21 @@ const SuspectRevealDTO = require('../domain/DTO/response/SuspectRevealDTO');
  */
 exports.nightTimeEvents = (io, socket, mafiaGame) => {
     socket.on('mafia-vote', (mafiaVoteObj) => {
-        // Get the Room instance for this player from their info
-        let room = mafiaGame.gameRoomsDict[socket.rooms[0]];
+        let room = socket.player.roomID;
         room.mafiaChosenPlayer = room.getPlayerByNickname(
             mafiaVoteObj.votingFor
         );
     });
 
     socket.on('medic-vote', (medicVoteObj) => {
-        let room = mafiaGame.gameRoomsDict[socket.rooms[0]];
+        let room = socket.player.roomID;
         room.medicChosenPlayer = room.getPlayerByNickname(
             mafiaVoteObj.votingFor
         );
     });
 
     socket.on('detective-vote', (detectiveVoteObj) => {
-        let room = mafiaGame.gameRoomsDict[socket.rooms[0]];
+        let room = socket.player.roomID;
 
         let suspect = room.getPlayerByNickname(detectiveVoteObj.votingFor);
         let isMafia = false;
