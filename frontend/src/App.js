53bb19e18bf6a68ed0bafbdd411b888f77ee1cd7
atha670 <61758990+atha670@users.noreply.github.com>
2021-03-15 16:44:10 +1300

    #27 features/game over screen (#121)
    * GameOver Screen
    
    * New branch created
    
    * git ignore of DS_STore
    
    * #27 General Placeholders for both text and picture
    
    * Slight style  changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,5 @@
 import './App.css';
-import HomeScreenID from './Components/HomeScreenID';
 
-function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <div style={{ width: '80%' }}>
-                    <HomeScreenID/>
-                </div>
-            </header>
         </div>
     );
 }
