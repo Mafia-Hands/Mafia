df1af5bd3c27f80313246fdc4907d4525aa581ec
dh <45830988+Dhors@users.noreply.github.com>
2021-03-16 02:38:49 +1300

    Created event handlers for lobby full and confirm start game and tests. (#104)
    * added event handler for lobby full and confirm start game. Also fixed bug with room.players array not being made correctly
    
    * removed conflicting parts
    
    * removed test and added host field to room - not used right now

--- backend/test/index.test.js
+++ backend/test/index.test.js
@@ -1,85 +1,103 @@
-const Client = require('socket.io-client');
-const CreateLobbyDTO = require('../domain/DTO/request/CreateLobbyDTO');
-const config = require('../config.json');
-const SocketIOServer = require('../index');
+const Client = require("socket.io-client");
+const CreateLobbyDTO = require("../domain/DTO/request/CreateLobbyDTO");
+const config = require("../config.json");
+const SocketIOServer = require("../index");
 
-describe('Create-lobby event test', () => {
-    let clientSocket;
-    const port = process.env.PORT || config.local_port;
+describe("Create-lobby event test", () => {
+  let clientSocket;
+  const port = process.env.PORT || config.local_port;
 
-    beforeEach((done) => {
-        clientSocket = new Client(`http://localhost:` + port);
-        clientSocket.on('connect', done);
+  beforeEach((done) => {
+    clientSocket = new Client(`http://localhost:` + port);
+    clientSocket.on("connect", done);
+  });
+
+  // Disconnect each socket connected to the server
+  afterEach((done) => {
+    var sockets = SocketIOServer.io.sockets.sockets;
+
+    // Iterate through each connected client and disconnect them.
+    sockets.forEach(function (socket, key) {
+      socket.disconnect(true);
     });
 
-    // Disconnect each socket connected to the server
-    afterEach((done) => {
-        var sockets = SocketIOServer.io.sockets.sockets;
+    done();
+  });
 
-        // Iterate through each connected client and disconnect them.
-        sockets.forEach(function (socket, key) {
-            socket.disconnect(true);
-        });
+  // Close the server once all tests are done
+  afterAll(() => {
+    SocketIOServer.server.close();
+  });
 
+  test("Simple create lobby events", (done) => {
+    let createLobbyDTO = new CreateLobbyDTO("Anmol");
+
+    // Subscribe to lobby-code
+    clientSocket.on("lobby-code", (lobbyCodeDTO) => {
+      expect(lobbyCodeDTO.code).toBeDefined();
+      done();
+    });
+
+    // Request to create a new lobby
+    clientSocket.emit("create-lobby", createLobbyDTO);
+  });
+
+  test("Two hosts with two rooms", (done) => {
+    let createLobbyDTO = new CreateLobbyDTO("Anmol");
+
+    // Client 1 to subscribe to lobby-code
+    clientSocket.on("lobby-code", (lobbyCodeDTO) => {
+      expect(lobbyCodeDTO.code).toBeDefined();
+      // Wait for the other client to throw errors, if there are any.
+      setTimeout(function () {
         done();
+      }, 1000);
     });
 
-    // Close the server once all tests are done
-    afterAll(() => {
-        SocketIOServer.server.close();
+    let clientSocket2 = new Client(`http://localhost:4001`);
+    // Client 2 to subscribe to lobby-code
+    clientSocket2.on("lobby-code", (lobbyCodeDTOString) => {
+      throw new Error("Client 2 shouldn't receive a lobby code");
     });
 
-    test('Simple create lobby events', (done) => {
-        let createLobbyDTO = new CreateLobbyDTO('Anmol');
+    // Request to create a new lobby
+    clientSocket.emit("create-lobby", createLobbyDTO);
+  });
 
-        // Subscribe to lobby-code
-        clientSocket.on('lobby-code', (lobbyCodeDTO) => {
-            expect(lobbyCodeDTO.code).toBeDefined();
-            done();
-        });
+  test("Host starts game event", (done) => {
+    let createLobbyDTO = new CreateLobbyDTO("Anmol");
 
-        // Request to create a new lobby
-        clientSocket.emit('create-lobby', createLobbyDTO);
+    // Request to create a new lobby
+    // this will set the socket id to the host to be tested.
+    clientSocket.emit("create-lobby", createLobbyDTO);
+
+    // Subscribe to game start event
+    // server should send this event back once host clicks start.
+    // Client to subscribe to "game-start"
+    clientSocket.on("game-start", () => {
+      done();
     });
 
-    test('Two hosts with two rooms', (done) => {
-        let createLobbyDTO = new CreateLobbyDTO('Anmol');
+    // Request to server that game is ready to be started.
+    clientSocket.emit("start-game");
+  });
 
-        // Client 1 to subscribe to lobby-code
-        clientSocket.on('lobby-code', (lobbyCodeDTO) => {
-            expect(lobbyCodeDTO.code).toBeDefined();
-            // Wait for the other client to throw errors, if there are any.
-            setTimeout(function () {
-                done();
-            }, 1000);
-        });
+  test("Reset lobby", (done) => {
+    let createLobbyDTO = new CreateLobbyDTO("Anmol");
 
-        let clientSocket2 = new Client(`http://localhost:4001`);
-        // Client 2 to subscribe to lobby-code
-        clientSocket2.on('lobby-code', (lobbyCodeDTOString) => {
-            throw new Error("Client 2 shouldn't receive a lobby code");
-        });
-
-        // Request to create a new lobby
-        clientSocket.emit('create-lobby', createLobbyDTO);
+    // Subscribe to lobby-code
+    clientSocket.on("lobby-code", (lobbyCodeDTO) => {
+      expect(lobbyCodeDTO.code).toBeDefined();
+      // Only emit reset-lobby once lobby code has been received
+      clientSocket.emit("reset-lobby");
     });
 
-    test('Reset lobby', (done) => {
-        let createLobbyDTO = new CreateLobbyDTO('Anmol');
-
-        // Subscribe to lobby-code
-        clientSocket.on('lobby-code', (lobbyCodeDTO) => {
-            expect(lobbyCodeDTO.code).toBeDefined();
-            // Only emit reset-lobby once lobby code has been received
-            clientSocket.emit('reset-lobby');
-        });
-
-        // Finish test once reset-lobby-update has been received
-        clientSocket.on('reset-lobby-update', () => {
-            done();
-        });
-
-        // Request to create a new lobby
-        clientSocket.emit('create-lobby', createLobbyDTO);
+    // Finish test once reset-lobby-update has been received
+    clientSocket.on("reset-lobby-update", () => {
+      done();
     });
+
+    // Request to create a new lobby
+    clientSocket.emit("create-lobby", createLobbyDTO);
+  });
 });
