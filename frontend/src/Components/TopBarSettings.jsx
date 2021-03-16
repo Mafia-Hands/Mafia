b3905758f2821fc865b55607aee1876b853bcaa7
reeved <dcunhareeve@gmail.com>
2021-03-17 02:26:23 +1300

    Fix/front end linting fixes (#134)
    This PR adds ESLint back to the Frontend and fixes errors due to linting checks.

--- frontend/src/Components/TopBarSettings.jsx
+++ frontend/src/Components/TopBarSettings.jsx
@@ -1,42 +1,40 @@
 import { React } from 'react';
-import styles from '../Styles/TopBarSettings.module.css';
+import PropTypes from 'prop-types';
 import { Button } from '@material-ui/core';
+import styles from '../Styles/TopBarSettings.module.css';
 
 /**
  * @param currentScreen true/false OPTIONAL prop that will current screen Header text if true
  * @param showSettings true/false OPTIONAL prop that will render settings button if true
  */
-const TopBarSettings = ({
-    currentScreen,
-    showSettings,
-    showUp,
-    setOpenInfo,
-}) => {
-    return (
-        <div className={styles.container}>
-            {showUp && (
-                <Button
-                    variant="contained"
-                    className={styles.backButton}
-                    onClick={() => showUp(false)}
-                >
-                    Go Back
-                </Button>
-            )}
-            {currentScreen && (
-                <h2 className={styles.header}>{`${currentScreen}`}</h2>
-            )}
-            {showSettings && (
-                <Button
-                    variant="contained"
-                    className={styles.settingsButton}
-                    onClick={() => setOpenInfo(false)}
-                >
-                    Settings
-                </Button>
-            )}
-        </div>
-    );
-};
+const TopBarSettings = ({ currentScreen, showSettings, showUp, setOpenInfo }) => (
+    <div className={styles.container}>
+        {showUp && (
+            <Button variant="contained" className={styles.backButton} onClick={() => showUp(false)}>
+                Go Back
+            </Button>
+        )}
+        {currentScreen && <h2 className={styles.header}>{`${currentScreen}`}</h2>}
+        {showSettings && (
+            <Button variant="contained" className={styles.settingsButton} onClick={() => setOpenInfo(false)}>
+                Settings
+            </Button>
+        )}
+    </div>
+);
 
 export default TopBarSettings;
+
+TopBarSettings.propTypes = {
+    currentScreen: PropTypes.string,
+    showSettings: PropTypes.bool,
+    showUp: PropTypes.func,
+    setOpenInfo: PropTypes.func,
+};
+
+TopBarSettings.defaultProps = {
+    currentScreen: null,
+    showSettings: null,
+    showUp: null,
+    setOpenInfo: null,
+};
--- frontend/src/Pages/SettingDialog.js
+++ frontend/src/Pages/SettingDialog.js
@@ -1,28 +1,26 @@
 import React from 'react';
-import styles from '../Styles/SettingDialog.module.css';
 import { Slider, Button } from '@material-ui/core';
+import styles from '../Styles/SettingDialog.module.css';
 
-const SettingDialog = () => {
-    return (
-        <div className={styles.container}>
-            <div>
-                <b> Sound </b>
-                <Slider />
-            </div>
-
-            <div>
-                <b> Brightness </b>
-                <Slider />
-            </div>
-
-            {/* TODO: improve CSS and layout */}
-            <div>
-                <Button variant="contained" style={{ textAlign: 'center' }}>
-                    Leave Game
-                </Button>
-            </div>
+const SettingDialog = () => (
+    <div className={styles.container}>
+        <div>
+            <b> Sound </b>
+            <Slider />
         </div>
-    );
-};
+
+        <div>
+            <b> Brightness </b>
+            <Slider />
+        </div>
+
+        {/* TODO: improve CSS and layout */}
+        <div>
+            <Button variant="contained" style={{ textAlign: 'center' }}>
+                Leave Game
+            </Button>
+        </div>
+    </div>
+);
 
 export default SettingDialog;
