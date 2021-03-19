ed9b40c34640009b49ae773ca4c93a4e7fe74bd1
SkyN <49630838+skyn310198@users.noreply.github.com>
2021-03-20 00:26:11 +1300

    Fix setting button in Game Over screen

--- frontend/src/Pages/GameOverPage.jsx
+++ frontend/src/Pages/GameOverPage.jsx
@@ -1,18 +1,56 @@
-import { React, useContext } from 'react';
+import { React, useState, useContext } from 'react';
 import TopBarSettings from '../Components/TopBarSettings';
 import Player from '../Components/Player';
 import { GameContext } from './GamePage';
+import ModalMUI from '../Modal/ModalMUI';
+import SettingDialog from '../Pages/SettingDialog';
+import { Button } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
 
-const GameOverPage = ({ userName, role }) => {
+const GameOverPage = () => {
     const { state } = useContext(GameContext);
+    const [open, setOpen] = useState(false);
+
     return (
-        <div>
-            <TopBarSettings currentScreen={`WINNER are a ${state.winningRole}`} showSettings={true} />
+        <div> 
+            <Grid container direction='row'>
+            <Grid item xs={0} sm={2}></Grid>
+            <Grid item xs={10} sm={8}><h1>{`WINNER are a ${state.winningRole}`}</h1></Grid>
+
+            <Grid item xs={0} sm={2}>
+                <Button
+                    variant="contained"
+                    onClick={() => {
+                        setOpen(true);
+                    }}
+                >
+                    Settings
+                </Button>
+            </Grid>      
+            
+            </Grid>
+            
             <div>
                 {state.winners.map((name) => {
                     return <Player playerName={name} />;
                 })}
             </div>
+
+            <div>
+                <ModalMUI open={open} setOpen={setOpen}>
+                    <div>
+                        <TopBarSettings
+                            showBack={true}
+                            showUp={setOpen}
+                            currentScreen="SETTINGS"
+                            showSettings={false}
+                        />
+                        <SettingDialog />
+                    </div>
+                </ModalMUI>
+            </div>
+
+
         </div>
     );
 };
