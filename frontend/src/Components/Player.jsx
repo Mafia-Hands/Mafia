ad545950bfa79b5c437d4d88101ede8e91f86a9a
dhors <smexylure@gmail.com>
2021-03-19 19:11:39 +1300

    show medic/mafia/detect who they have voted for in night ability, as well as show who people have voted for in trial.

--- frontend/src/Components/Player.jsx
+++ frontend/src/Components/Player.jsx
@@ -5,28 +5,30 @@ import socket from '../Socket';
 import { useContext } from 'react';
 
 export default function Player({ playerId, playerName, style, childRef }) {
-    const { state: gameState } = useContext(GameContext);
+    const { state: gameState, dispatch } = useContext(GameContext);
 
     const isDead = !gameState.alivePlayers.includes(playerName);
 
     const isHoverable =
         !!gameState.votingState.type && gameState.votingState.votablePlayers.includes(playerName) && !isDead;
     const hasVoted = gameState.votingState.playersWhoVoted.includes(playerName);
-    const isClicked = !!gameState.votingState.vote;
+
+    const isVoted = (gameState.votingState.vote === playerName);
+
 
     // apply styles based on whether certain props is true
     const playerStyle = classNames({
         [styles.playerWrapper]: true,
         [styles.isHoverable]: isHoverable,
         [styles.hasVoted]: hasVoted,
-        [styles.isClicked]: isClicked,
+        [styles.isClicked]: isVoted,
         [styles.isDead]: isDead,
     });
 
     // this only allows clicks if a player is actually hoverable.
     function validateOnClick(fn) {
         return (...args) => {
-            if (!isDead && isHoverable && !isClicked) {
+            if (!isDead && isHoverable && !isVoted) {
                 fn(...args);
             }
         };
@@ -38,12 +40,15 @@ export default function Player({ playerId, playerName, style, childRef }) {
                 socket.emit(`${gameState.role}-vote`, {
                     votingFor: playerName,
                 });
+                dispatch( { type:'show-selected' , status: `Selected ${playerName} for ability`, votedPlayer: playerName } );
                 break;
             case 'discussion':
                 socket.emit(`day-vote`, { votingFor: playerName });
+                dispatch( { type:'show-selected' , status: `Voted ${playerName} for trial`, votedPlayer: playerName } );
                 break;
             case 'trial':
                 socket.emit(`trial-vote`, { votingFor: playerName });
+                dispatch( { type:'show-selected' , status: `Voted to hang ${playerName}`, votedPlayer: playerName } );
                 break;
             default:
                 throw new Error('Invalid voting type');
