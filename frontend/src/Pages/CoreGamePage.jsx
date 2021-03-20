33a71f10167f7ece428f025a6a2fc0f60a578728
skyn310198 <49630838+skyn310198@users.noreply.github.com>
2021-03-20 21:12:00 +1300

    Add functionality to Bottom Bar (#195)
    * Add functionality to Bottom Bar - able to show current Day/Night of the game
    
    * change the way to implement bottom bar
    
    * remove husky
    
    * minor changes
    
    * change back to heroku

--- frontend/src/Pages/CoreGamePage.jsx
+++ frontend/src/Pages/CoreGamePage.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import {React} from 'react';
 import TopBarGame from '../Components/TopBarGame';
 import GameEnv from '../Components/GameEnv';
 import BottomBar from '../Components/BottomBar';
+
 
 export default function CoreGamePage() {
     return (
         <div>
             <TopBarGame showTimer={true} showRole={true} />
             <GameEnv />
-            <BottomBar />
+            <BottomBar/>
         </div>
     );
 }
