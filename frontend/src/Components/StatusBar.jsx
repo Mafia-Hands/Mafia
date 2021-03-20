29c4bce8b0d1d5d60d93df5a46d60d2b4f8d63e1
Kevin Ge <gjw1047340859@gmail.com>
2021-03-20 17:01:04 +1300

    add fade in animation on entry sceen, game over sceen, and status bar add zoom effect on hoverable circle in the table

--- frontend/src/Components/StatusBar.jsx
+++ frontend/src/Components/StatusBar.jsx
@@ -1,12 +1,22 @@
 import React, { useContext } from 'react';
 import { GameContext } from '../Pages/GamePage';
 import styles from '../Styles/Statusbar.module.css';
+import { animated, useSpring } from 'react-spring';
 
 export default function StatusBar() {
     const { state } = useContext(GameContext);
+    const props = useSpring({
+        to: { opacity: 1 },
+        from: { opacity: 0.3 },
+        delay: 2000,
+        reset: true,
+    });
+
     return (
         <div>
-            <h1 className={styles.heading}> {state.status}</h1>
+            <animated.div style={props}>
+                <h1 className={styles.heading}> {state.status}</h1>
+            </animated.div>
         </div>
     );
 }
