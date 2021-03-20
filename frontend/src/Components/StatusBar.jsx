7dd5595e16ddda52c7d4b860b27239851ac85f0e
Kevin Ge <gjw1047340859@gmail.com>
2021-03-20 18:19:01 +1300

    fix the fadding in issue in status bar

--- frontend/src/Components/StatusBar.jsx
+++ frontend/src/Components/StatusBar.jsx
@@ -1,22 +1,15 @@
 import React, { useContext } from 'react';
 import { GameContext } from '../Pages/GamePage';
 import styles from '../Styles/Statusbar.module.css';
-import { animated, useSpring } from 'react-spring';
 
 export default function StatusBar() {
     const { state } = useContext(GameContext);
-    const props = useSpring({
-        to: { opacity: 1 },
-        from: { opacity: 0.3 },
-        delay: 2000,
-        reset: true,
-    });
 
     return (
         <div>
-            <animated.div style={props}>
-                <h1 className={styles.heading}> {state.status}</h1>
-            </animated.div>
+            <h1 key={state.status} className={styles.heading}>
+                {state.status}
+            </h1>
         </div>
     );
 }
