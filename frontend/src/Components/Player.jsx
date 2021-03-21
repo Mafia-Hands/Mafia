a3294183ca75b9282294612f252c8fe517a8b24b
tguernier <tgue760@aucklanduni.ac.nz>
2021-03-21 15:20:44 +1300

    forgot a console.log

--- frontend/src/Components/Player.jsx
+++ frontend/src/Components/Player.jsx
@@ -17,7 +17,6 @@ export default function Player({ playerId, playerName, style, childRef }) {
 
     // forces the detctive to only be able to look at one other player per day
     const detectiveHasSuspected = (gameState.votingState.vote !== '')
-    console.log(detectiveHasSuspected)
 
     var mafiaString = ""
     for (const suspectedPlayer of gameState.checkedPlayers) {
