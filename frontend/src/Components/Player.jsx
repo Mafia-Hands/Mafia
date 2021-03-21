b219b08085fc48b82f9a2873b8240b41c4fe4ba7
Reeve <dcunhareeve@gmail.com>
2021-03-21 22:11:01 +1300

    Better styling for Players on table

--- frontend/src/Components/Player.jsx
+++ frontend/src/Components/Player.jsx
@@ -1,33 +1,36 @@
 import styles from '../Styles/Player.module.css';
 import classNames from 'classnames';
 import { GameContext } from '../Pages/GamePage';
+import { GeneralContext } from '../App';
 import socket from '../Socket';
 import { useContext } from 'react';
 
 export default function Player({ playerId, playerName, style, childRef }) {
     const { state: gameState, dispatch } = useContext(GameContext);
+    const { state: generalState } = useContext(GeneralContext);
 
     const isDead = !gameState.alivePlayers.includes(playerName);
-
     const isHoverable =
         !!gameState.votingState.type && gameState.votingState.votablePlayers.includes(playerName) && !isDead;
     const hasVoted = gameState.votingState.playersWhoVoted.includes(playerName);
-
-    const isVoted = (gameState.votingState.vote === playerName);
+    const isVoted = gameState.votingState.vote === playerName;
+    const isPlayer = generalState.nickname === playerName;
+    console.log('Player nickname:', generalState.nickname, 'PlayerName: ', playerName);
 
     // forces the detctive to only be able to look at one other player per day
-    const detectiveHasSuspected = (gameState.votingState.vote !== '')
+    const detectiveHasSuspected = gameState.votingState.vote !== '';
 
-    var mafiaString = ""
+    var mafiaString = '';
     for (const suspectedPlayer of gameState.checkedPlayers) {
         if (suspectedPlayer.nickname === playerName) {
-            mafiaString = (suspectedPlayer.isMafia ? " (Mafia)" : " (Not Mafia)");
+            mafiaString = suspectedPlayer.isMafia ? ' (Mafia)' : ' (Not Mafia)';
         }
     }
 
     // apply styles based on whether certain props is true
     const playerStyle = classNames({
         [styles.playerWrapper]: true,
+        [styles.player]: isPlayer,
         [styles.isHoverable]: isHoverable,
         [styles.hasVoted]: hasVoted,
         [styles.isClicked]: isVoted,
@@ -46,21 +49,25 @@ export default function Player({ playerId, playerName, style, childRef }) {
     function onClick() {
         switch (gameState.votingState.type) {
             case 'role':
-                if (!((gameState.role === 'detective') && detectiveHasSuspected)) {
+                if (!(gameState.role === 'detective' && detectiveHasSuspected)) {
                     socket.emit(`${gameState.role}-vote`, {
                         votingFor: playerName,
                     });
-                    dispatch( { type:'show-selected' , status: `Selected ${playerName} for ability`, votedPlayer: playerName } );
+                    dispatch({
+                        type: 'show-selected',
+                        status: `Selected ${playerName} for ability`,
+                        votedPlayer: playerName,
+                    });
                     break;
                 }
                 break;
             case 'discussion':
                 socket.emit(`day-vote`, { votingFor: playerName });
-                dispatch( { type:'show-selected' , status: `Voted ${playerName} for trial`, votedPlayer: playerName } );
+                dispatch({ type: 'show-selected', status: `Voted ${playerName} for trial`, votedPlayer: playerName });
                 break;
             case 'trial':
                 socket.emit(`trial-vote`, { votingFor: playerName });
-                dispatch( { type:'show-selected' , status: `Voted to kill ${playerName}`, votedPlayer: playerName } );
+                dispatch({ type: 'show-selected', status: `Voted to kill ${playerName}`, votedPlayer: playerName });
                 break;
             default:
                 throw new Error('Invalid voting type');
@@ -70,7 +77,7 @@ export default function Player({ playerId, playerName, style, childRef }) {
     return (
         <div className={playerStyle} style={style} ref={childRef} onClick={validateOnClick(onClick)}>
             <div className={styles.playerText}>
-                <p>{playerName.concat(isDead ? " (DEAD)" : "")}</p>
+                <p>{playerName.concat(isDead ? ' (DEAD)' : '')}</p>
                 <p>{mafiaString}</p>
             </div>
         </div>
