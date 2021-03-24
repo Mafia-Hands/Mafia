d32733ad4d044227b3c1196dd8c28c32cf89e52d
Reeve <dcunhareeve@gmail.com>
2021-03-24 17:53:40 +1300

    small code refactor

--- frontend/src/Components/GameEnv.jsx
+++ frontend/src/Components/GameEnv.jsx
@@ -1,7 +1,6 @@
 import { React } from 'react';
 import StatusBar from './StatusBar';
 import Table from './Table';
-// import styles from '../Styles/GameEnv.module.css';
 
 const GameEnv = () => (
     <>
