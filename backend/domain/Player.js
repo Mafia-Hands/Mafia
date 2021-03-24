8e56426d28526431a3e7c8b6b596d7ffdbcdf9fb
Seif Younes <seif99y@gmail.com>
2021-03-25 12:42:39 +1300

    Commenting for domain objects (#254)
    * Added JSDoc comments to methods in Room class
    
    * Added JSdoc comments to VoteHandler
    
    * Added JSDoc comments to MafiaGame, Player and Room class definitions
    
    * Replaced comments for domain Enums with JSdoc
    
    * Removed break statement i added in previous commit

--- backend/domain/Player.js
+++ backend/domain/Player.js
@@ -1,3 +1,8 @@
+/**
+ * Represents a player in a game of Mafia.
+ * A player is part of a room, and they are tied to a socket that is connected to the server.
+ * Players are identified by a unique nickname within their room.
+ */
 class Player {
     constructor(socketID, roomID, nickname, role, isHost) {
         this.socketID = socketID;
@@ -8,6 +13,9 @@ class Player {
         this.isHost = isHost;
     }
 
+    /**
+     * Resets the state of a player by bringing them back to life with a 'null' role.
+     */
     resetPlayer() {
         this.role = null;
         this.isAlive = true;
