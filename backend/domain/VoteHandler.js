8f2a05e757852300f7bee57a4851931060494526
Seif Younes <seif99y@gmail.com>
2021-03-25 00:11:52 +1300

    Added JSdoc comments to VoteHandler

--- backend/domain/VoteHandler.js
+++ backend/domain/VoteHandler.js
@@ -1,8 +1,12 @@
 /**
  * A class used to handle any voting related events or calculations for a Room.
  * This includes tallying votes, and figuring out who the most voted player is.
+ * The vote handler keeps track of vote maps. These are JSON objects where each key is a player nickname, and each value is the Player object that was voted for.
  */
 class VoteHandler {
+    /**
+     * Constructor: mainly just initialises the vote maps as empty JSON objects.
+     */
     constructor() {
         this.daytimeVoteMap = {};
         this.mafiaVoteMap = {};
@@ -10,6 +14,11 @@ class VoteHandler {
         this.medicChosenPlayer = null;
     }
 
+    /**
+     * Gets the player voted for by the mafia, based on the mafia vote map.
+     * Compares the voted player with the player that the medic has chosen to save, to check if they are the same player.
+     * @returns The player that has been killed by the mafia, or null if the medic has saved the right player.
+     */
     getMafiaVotedPlayer() {
         const mafiaChosenPlayer = this.getVotedPlayer(this.mafiaVoteMap);
         if (this.medicChosenPlayer && mafiaChosenPlayer === this.medicChosenPlayer.nickname) {
@@ -18,10 +27,18 @@ class VoteHandler {
         return mafiaChosenPlayer;
     }
 
+    /**
+     * Gets the most voted player from the discussion period of the game.
+     * @returns The player selected for trial.
+     */
     getDaytimeVotedPlayer() {
         return this.getVotedPlayer(this.daytimeVoteMap);
     }
 
+    /**
+     * Gets the most voted player from the trial period of the game.
+     * @returns The player that has been executed in the trial.
+     */
     getTrialVotedPlayer() {
         return this.getVotedPlayer(this.trialVoteMap, true);
     }
