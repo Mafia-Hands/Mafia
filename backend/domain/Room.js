d13b5ec55f87228cc456f5aa27535ea1ac98d655
Seif Younes <seif99y@gmail.com>
2021-03-25 00:03:11 +1300

    Added JSDoc comments to methods in Room class

--- backend/domain/Room.js
+++ backend/domain/Room.js
@@ -21,18 +21,29 @@ class Room {
         this.host = null;
     }
 
+    /**
+     * Gets the player in the game matching the specified nickname.
+     * @param {String} nickname The player's nickname
+     * @returns The player with the specified nickname, or 'undefined' if they are not found.
+     */
     getPlayerByNickname(nickname) {
         let returnedPlayer;
 
         this.players.forEach((player) => {
             if (player.nickname === nickname) {
                 returnedPlayer = player;
+                break;
             }
         });
 
         return returnedPlayer;
     }
 
+    /**
+     * Get all players matching a specified role
+     * @param {Role} role
+     * @returns All players with that role, or an empty list if none are found.
+     */
     getPlayersByRole(role) {
         const playersOfTheRole = [];
         this.players.forEach((player) => {
@@ -44,6 +55,11 @@ class Room {
         return playersOfTheRole;
     }
 
+    /**
+     * Gets the winning players for the game, given a role. If the specified role is CIVILIAN, medics and detectives are included.
+     * @param {Role} role The winning role: either CIVILIAN, MAFIA or JESTER
+     * @returns The list of winning players , depending on the winning role provided.
+     */
     getWinningPlayers(role) {
         let playersOfTheRole = [];
         if (role === roles.CIVILIAN) {
@@ -58,16 +74,27 @@ class Room {
         return playersOfTheRole;
     }
 
+    /**
+     * Add a new player to the game. The player is not added if the max player count has already been met.
+     * @param {Player} player The player to add to the game.
+     */
     addPlayer(player) {
         if (player !== null && this.players.length < this.maxPlayerCount) {
             this.players.push(player);
         }
     }
 
+    /**
+     * Moves on to the next round of this room's game.
+     */
     incrementRoundNumber() {
         this.roundNumber += 1;
     }
 
+    /**
+     * Checks the win conditions for each role to determine which role has won the game, if any.
+     * @returns The winning role, or null if no role has won.
+     */
     getWinningRole() {
         const numOfMafiaAlive = this.getPlayersByRole(roles.MAFIA).filter((player) => player.isAlive).length;
         const numOfJesterAlive = this.getPlayersByRole(roles.JESTER).filter((player) => player.isAlive).length;
@@ -90,6 +117,9 @@ class Room {
         return null;
     }
 
+    /**
+     * Reset the game state. Used when the game for this room is restarted, or when the game ends.
+     */
     resetGame() {
         this.gameState = INITIAL_GAME_STATE;
         this.roundNumber = INITIAL_ROUND_NUMBER;
