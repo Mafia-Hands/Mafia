0593dcbe1f1ff520169b46ba67cbe19c96c49493
Yicheng Sun <lhsunyc@outlook.com>
2021-04-06 13:22:41 +0800

    Add an enum for abstain vote
    "abstain vote" is renamed to "no confidence vote" to prevent ambiguous meaning.

--- backend/domain/VoteHandler.js
+++ backend/domain/VoteHandler.js
@@ -3,6 +3,8 @@
  * This includes tallying votes, and figuring out who the most voted player is.
  * The vote handler keeps track of vote maps. These are JSON objects where each key is a player nickname, and each value is the Player object that was voted for.
  */
+const votes = require('./Enum/Vote');
+
 class VoteHandler {
     /**
      * Constructor: mainly just initialises the vote maps as empty JSON objects.
@@ -47,10 +49,10 @@ class VoteHandler {
     getVotedPlayer(voteMap, isTrial) {
         // Generate map of players who have been voted for, and the number of votes they have.
         const voteTally = {};
-        voteTally['abstain Vote'] = 0;
+        voteTally[votes.NoConfidenceVote] = 0;
         for (const [, chosenPlayer] of Object.entries(voteMap)) {
-            if (chosenPlayer === 'abstain Vote') {
-                voteTally['abstain Vote'] += 1;
+            if (chosenPlayer === votes.NoConfidenceVote) {
+                voteTally[votes.NoConfidenceVote] += 1;
             } else if (Object.prototype.hasOwnProperty.call(voteTally, chosenPlayer.nickname)) {
                 voteTally[chosenPlayer.nickname] += 1;
             } else {
@@ -62,7 +64,7 @@ class VoteHandler {
         let maxVotes = 0;
         let votedPlayer = null;
         for (const [player, numVotes] of Object.entries(voteTally)) {
-            if (numVotes > maxVotes || (isTrial && player === 'abstain Vote' && numVotes === maxVotes)) {
+            if (numVotes > maxVotes || (isTrial && player === votes.NoConfidenceVote && numVotes === maxVotes)) {
                 maxVotes = numVotes;
                 votedPlayer = player;
             }
