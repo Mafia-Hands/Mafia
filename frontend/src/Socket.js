7e584824499437460e9682da30979ee35c2db76f
Harpreet Singh <harpreetsingh@Harpreets-MacBook-Pro.local>
2021-04-06 13:03:43 +1200

    change Socket.js to use default server

--- frontend/src/Socket.js
+++ frontend/src/Socket.js
@@ -2,9 +2,9 @@ import socketIOClient from 'socket.io-client';
 
 /* eslint-disable new-cap */
 // Uncomment if you would like to use the deployed server
-// const socket = new socketIOClient('mafia-hands-backend-cd.herokuapp.com');
+const socket = new socketIOClient('mafia-hands-backend-cd.herokuapp.com');
 
 // Uncomment if you would like to use a local server to test server changes
-const socket = new socketIOClient('localhost:4001');
+// const socket = new socketIOClient('localhost:4001');
 
 export default socket;
