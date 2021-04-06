9c3ce048ca5f6e2cdb8c85582c46dc5557f75cb0
Daniel <detz582@aucklanduni.ac.nz>
2021-04-06 18:26:27 +1200

    Update StateChangeEvents.integration.test.js
    Added 7 player versions of the integration tests that currently test 6 players.

--- backend/test/integration/StateChangeEvents.integration.test.js
+++ backend/test/integration/StateChangeEvents.integration.test.js
@@ -67,6 +67,43 @@ describe('NightStateChangeEvents integration tests', () => {
         done();
     });
 
+    test('integration test start-night 7 players', async (done) => {
+        // Start the night
+        async function startNight() {
+            return new Promise((resolve) => {
+                let socketResponseCount = 0;
+                // Start the night when response retrieved
+                clientSockets[0].emit('start-night');
+                // Attach handlers to night-start and night-end. In total should be 12 responses
+                for (let i = 0; i < 7; i += 1) {
+                    clientSockets[i].once('night-start', (nightStartDTO) => {
+                        expect(nightStartDTO.timeToVote).toBeDefined();
+                        socketResponseCount += 1;
+                        if (socketResponseCount >= 14) {
+                            resolve();
+                        }
+                    });
+                    clientSockets[i].once('night-end', (nightEndDTO) => {
+                        // Player killed should be null, as no one voted
+                        expect(nightEndDTO.playerKilled).toBeNull();
+                        socketResponseCount += 1;
+                        if (socketResponseCount >= 14) {
+                            resolve();
+                        }
+                    });
+                }
+            });
+        }
+
+        for (let i = 1; i < 7; i += 1) {
+            await connectAndJoin(clientSockets, i, port, lobbyCode);
+        }
+        await startGame(clientSockets);
+        await startNight();
+
+        done();
+    });
+
     test('integration test night-start 1 player', async (done) => {
         // Single player should immediately win as only player left
         // May be invalid in the future if we decide to enforce starts with more than 3 players
@@ -137,6 +174,43 @@ describe('DayStateEvents integration tests', () => {
 
         done();
     });
+
+    test('integration test start-day 7 players', async (done) => {
+        // Start the day
+        async function startDay() {
+            return new Promise((resolve) => {
+                let socketResponseCount = 0;
+                // Start the day when response retrieved
+                clientSockets[0].emit('start-day');
+                // Attach handlers to day-start and discussion-end. In total should be 12 responses
+                for (let i = 0; i < 7; i += 1) {
+                    clientSockets[i].once('day-start', (dayStartDTO) => {
+                        expect(dayStartDTO.timeToVote).toBeDefined();
+                        socketResponseCount += 1;
+                        if (socketResponseCount >= 14) {
+                            resolve();
+                        }
+                    });
+                    clientSockets[i].once('discussion-end', (discussionEndDTO) => {
+                        // Player chosen should be null, as no one voted
+                        expect(discussionEndDTO.playerOnTrial).toBeNull();
+                        socketResponseCount += 1;
+                        if (socketResponseCount >= 14) {
+                            resolve();
+                        }
+                    });
+                }
+            });
+        }
+
+        for (let i = 1; i < 7; i += 1) {
+            await connectAndJoin(clientSockets, i, port, lobbyCode);
+        }
+        await startGame(clientSockets);
+        await startDay();
+
+        done();
+    });
 });
 
 describe('TrialStateChangeEvents integration tests', () => {
@@ -169,6 +243,43 @@ describe('TrialStateChangeEvents integration tests', () => {
         }
 
         for (let i = 1; i < 6; i += 1) {
+            await connectAndJoin(clientSockets, i, port, lobbyCode);
+        }
+        await startGame(clientSockets);
+        await startTrial();
+
+        done();
+    });
+
+    test('integration test start-trial 7 players', async (done) => {
+        // Start the trial
+        async function startTrial() {
+            return new Promise((resolve) => {
+                let socketResponseCount = 0;
+                // Start the trial when response retrieved
+                clientSockets[0].emit('start-trial');
+                // Attach handlers to trial-start and trial-end. In total should be 12 responses
+                for (let i = 0; i < 7; i += 1) {
+                    clientSockets[i].once('trial-start', (trialStartDTO) => {
+                        expect(trialStartDTO.timeToVote).toBeDefined();
+                        socketResponseCount += 1;
+                        if (socketResponseCount >= 14) {
+                            resolve();
+                        }
+                    });
+                    clientSockets[i].once('trial-end', (trialEndDTO) => {
+                        // Player killed should be null, as no one voted
+                        expect(trialEndDTO.playerKilled).toBeDefined();
+                        socketResponseCount += 1;
+                        if (socketResponseCount >= 14) {
+                            resolve();
+                        }
+                    });
+                }
+            });
+        }
+
+        for (let i = 1; i < 7; i += 1) {
             await connectAndJoin(clientSockets, i, port, lobbyCode);
         }
         await startGame(clientSockets);
