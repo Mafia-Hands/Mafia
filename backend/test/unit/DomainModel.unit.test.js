5fca48feb7247696c1e58aa2469a4108c3ebc8d7
RawiriHohepa <54158903+RawiriHohepa@users.noreply.github.com>
2021-04-11 19:04:52 +1200

    Loop over room.addPlayer unit test for all numbers of players

--- backend/test/unit/DomainModel.unit.test.js
+++ backend/test/unit/DomainModel.unit.test.js
@@ -3,64 +3,19 @@ const Room = require('../../domain/Room');
 const PlayerStatus = require('../../domain/enum/PlayerStatus');
 const Role = require('../../domain/enum/Role');
 
-describe('Domain Model tests', () => {
-    test('room domain six model object', (done) => {
-        const player1 = new Player('00001', 'room1', 'nickname1');
-        const player2 = new Player('00002', 'room1', 'nickname2');
-        const player3 = new Player('00003', 'room1', 'nickname3');
-        const player4 = new Player('00004', 'room1', 'nickname4');
-        const player5 = new Player('00005', 'room1', 'nickname5');
-        const player6 = new Player('00006', 'room1', 'nickname6');
+describe('Add Player tests', () => {
+    for (let playerCount = 6; playerCount <= 15; playerCount += 1) {
+        test('room add X players', () => {
+            const room = new Room();
 
-        const room = new Room();
+            for (let i = 1; i <= playerCount; i += 1) {
+                const player = new Player(`0000${i}`, `room${i}`, `nickname${i}`);
+                room.addPlayer(player);
+            }
 
-        room.addPlayer(player1);
-        room.addPlayer(player2);
-        room.addPlayer(player3);
-        room.addPlayer(player4);
-        room.addPlayer(player5);
-        room.addPlayer(player6);
-
-        if (room.players.length === 6) {
-            done();
-        }
-    });
-
-    test('room domain max model object', (done) => {
-        const player1 = new Player('00001', 'room1', 'nickname1');
-        const player2 = new Player('00002', 'room1', 'nickname2');
-        const player3 = new Player('00003', 'room1', 'nickname3');
-        const player4 = new Player('00004', 'room1', 'nickname4');
-        const player5 = new Player('00005', 'room1', 'nickname5');
-        const player6 = new Player('00006', 'room1', 'nickname6');
-        const player7 = new Player('00007', 'room1', 'nickname7');
-
-        const room = new Room();
-
-        room.addPlayer(player1);
-        room.addPlayer(player2);
-        room.addPlayer(player3);
-        room.addPlayer(player4);
-        room.addPlayer(player5);
-        room.addPlayer(player6);
-        room.addPlayer(player7);
-
-        if (room.players.length === 7) {
-            done();
-        }
-    });
-
-    test('room domain model 1 object', (done) => {
-        const player1 = new Player('00001', 'room1', 'nickname1');
-
-        const room = new Room();
-
-        room.addPlayer(player1);
-
-        if (room.players.length === 1) {
-            done();
-        }
-    });
+            expect(room.players.length).toBe(playerCount);
+        });
+    }
 });
 
 describe('winning role tests', () => {
